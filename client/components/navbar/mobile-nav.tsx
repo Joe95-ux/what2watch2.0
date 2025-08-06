f899491aa7c93cc36dc9347611236f080e2f662c
joe95-ux <ogorktabi@gmail.com>
2025-08-06 23:56:21 +0200

    add nav cptns

---
+++ client/components/navbar/mobile-nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mobile-nav() {
+  return (
+    <div>mobile-nav</div>
+  )
+}
+
+export default mobile-nav
---
+++ client/components/navbar/side-bar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const side-bar = () => {
+  return (
+    <div>side-bar</div>
+  )
+}
+
+export default side-bar
