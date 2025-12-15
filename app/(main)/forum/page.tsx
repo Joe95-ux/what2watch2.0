b362b3a1821f3b6584a2bffe10bca3541ee5ea73
joe95-ux <ogorktabi@gmail.com>
2025-12-15 07:38:40 +0200

    force forum page with dynamic force

--- app/(main)/forum/page.tsx
+++ app/(main)/forum/page.tsx
@@ -1,5 +1,8 @@
 import { ForumPageClient } from "@/components/forum/forum-page-client";
 
+// Force dynamic rendering since this page uses useSearchParams() for filtering
+export const dynamic = "force-dynamic";
+
 export default function ForumPage() {
   return <ForumPageClient />;
 }
