2149e8982d7c16d955e57c9e057f3f9105681043
joe95-ux <ogorktabi@gmail.com>
2025-12-18 09:44:19 +0200

    fix path aliases

--- next.config.ts
+++ next.config.ts
@@ -36,10 +36,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
 };
 
 export default nextConfig;
