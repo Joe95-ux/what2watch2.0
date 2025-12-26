f0d4e07227b34b34dc2c6ec6d497dc019d6b8833
joe95-ux <ogorktabi@gmail.com>
2025-12-27 00:29:27 +0200

    fix suspense bug on yt page

--- app/(main)/youtube/page.tsx
+++ app/(main)/youtube/page.tsx
@@ -1,5 +1,8 @@
 import { YouTubePageClient } from "@/components/youtube/youtube-page-client";
 
+// Force dynamic rendering since this page uses useSearchParams() for tab navigation
+export const dynamic = "force-dynamic";
+
 export default function YouTubePage() {
   return <YouTubePageClient />;
 }
